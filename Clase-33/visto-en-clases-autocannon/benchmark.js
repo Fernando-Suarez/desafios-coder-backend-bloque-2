6896165227fc1c02c552dc29b1cd16cc1b082d90
Fernando Suarez <jorgefernandosuarez@gmail.com>
2023-02-08 12:18:54 -0300

    visto en clases autocannon y 0x

---
+++ Clase-33/visto-en-clases-autocannon/benchmark.js
@@ -0,0 +1,23 @@
+const autocannon = require('autocannon');
+const { PassThrough } = require('stream');
+function run(url) {
+	const buf = [];
+	const outputStream = new PassThrough();
+	const inst = autocannon({
+		url,
+		connections: 100,
+		duration: 20,
+	});
+	autocannon.track(inst, { outputStream });
+	outputStream.on('data', (data) => buf.push(data));
+	inst.on('done', function () {
+		process.stdout.write(Buffer.concat(buf));
+	});
+}
+//comentar una sola para probar o cambiar los puertos para probar en simultaneo
+
+// console.log('Running benchmarks in bloq ...');
+// run('http://localhost:8080/auth-bloq?username=dani&password=qwerty123');
+
+console.log('Running benchmarks in nobloq ...');
+run('http://localhost:8080/auth-nobloq?username=dani&password=qwerty123');
